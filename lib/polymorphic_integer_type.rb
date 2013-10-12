32435e7625844c618889fce72763adb6b0ff55ae
Kyle d'Oliveira <kyle@goclio.com>
2013-10-11 20:22:51 -0700

    Need to add more dynamic methods and a new development gem dependencies

--- lib/polymorphic_integer_type.rb
+++ lib/polymorphic_integer_type.rb
@@ -1,25 +1,28 @@
 require "polymorphic_integer_type/version"
 
 module PolymorphicIntegerType
-  
+
   module ClassMethods
-    
+
     def belongs_to(name, options = {})
+      mapping = options.delete(:mapping)
       super
       if options[:polymorphic]
-        foreign_type = reflections[:poly].foreign_type
+        foreign_type = reflections[name].foreign_type
 
-        define_method :[] do |value|
-          if value == foreign_type
-            send(reflections[:poly].foreign_type)
-          else
-            super
-          end
-        end
-        
         define_method foreign_type do
-          t = super
-          options[:mapping][t]
+          t = super()
+          mapping[t]
+        end
+
+        define_method "#{foreign_type}=" do |klass|
+          enum = mapping.key(klass.to_s)
+          super(enum)
+        end
+
+        define_method "#{name}=" do |record|
+          super(record)
+          send("#{foreign_type}=", record.class)
         end
 
       end
@@ -29,7 +32,20 @@ module PolymorphicIntegerType
 
   def self.included(base)
     base.extend(ClassMethods)
-  end  
+  end
 
+  def _polymorphic_foreign_types
+    @_polymorphic_foreign_types ||= reflections.values.collect do
+      |assoc| assoc.foreign_type if assoc.options[:polymorphic]
+    end.compact
+  end
+
+  def [](value)
+    if _polymorphic_foreign_types.include?(value)
+      send(value)
+    else
+      super(value)
+    end
+  end
 
 end
--- polymorphic_integer_type.gemspec
+++ polymorphic_integer_type.gemspec
@@ -18,8 +18,11 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.3"
+  spec.add_development_dependency "bundler", "~> 1.2"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
-  
+  spec.add_development_dependency "activerecord", "3.2.11"
+  spec.add_development_dependency "mysql", "2.8.1"
+  spec.add_development_dependency "debugger"  
+
 end

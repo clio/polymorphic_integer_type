09745be9370f270076701de3d25572b9ad2bab90
Kyle d'Oliveira <kyle@goclio.com>
2015-01-09 16:32:46 -0800

    Updating the gemspec to use mysql2, activerecord 4.2 and byebug for ruby 2.1

--- spec/support/active_record.rb
+++ spec/support/active_record.rb
@@ -2,7 +2,7 @@ require 'active_record'
 Dir["#{File.dirname(__FILE__)}/migrations/*.rb"].each {|f| require f}
 
 config = {
-  :adapter => "mysql",
+  :adapter => "mysql2",
   :host => "localhost",
   :database => "polymorphic_integer_type_test",
   :username => "root",

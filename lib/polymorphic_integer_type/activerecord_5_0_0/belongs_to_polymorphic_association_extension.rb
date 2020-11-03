e20f2d0749b4180fe058f27b72f1fcbf72062bfb
Kyle d'Oliveira <kyle.doliveira@clio.com>
2020-11-03 14:25:55 -0800

    Need to better monkey patch the association method

--- lib/polymorphic_integer_type/activerecord_5_0_0/belongs_to_polymorphic_association_extension.rb
+++ lib/polymorphic_integer_type/activerecord_5_0_0/belongs_to_polymorphic_association_extension.rb
@@ -3,10 +3,16 @@ module ActiveRecord
     class BelongsToPolymorphicAssociation < BelongsToAssociation
       private def replace_keys(record)
         super
-        if record
-          owner[reflection.foreign_type] = record.class.base_class
+        if owner.respond_to?(:_polymorphic_foreign_types) && owner._polymorphic_foreign_types.include?(reflection.foreign_type)
+          if record
+            owner[reflection.foreign_type] = record.class
+          else
+            owner[reflection.foreign_type] = nil
+          end
+        elsif record.respond_to?(:polymorphic_name)
+          owner[reflection.foreign_type] = record.class.polymorphic_name
         else
-          owner[reflection.foreign_type] = nil
+          owner[reflection.foreign_type] = record.class.base_class.name
         end
       end
     end

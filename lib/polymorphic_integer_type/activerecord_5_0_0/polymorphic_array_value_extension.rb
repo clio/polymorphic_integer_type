bd110dc436d2383d41acaa6d73ae59aa7c588eb1
Drew Goddyn <drew.goddyn@clio.com>
2020-11-03 15:04:27 -0800

    With the introduction of #polymorphic_name we need additional checks to make sure our type mappings include namespaces. If they don't we can fall back to other methods like #sti_name, base_class.to_s, or base_class.sti_name until we get a hit.

--- lib/polymorphic_integer_type/activerecord_5_0_0/polymorphic_array_value_extension.rb
+++ lib/polymorphic_integer_type/activerecord_5_0_0/polymorphic_array_value_extension.rb
@@ -1,17 +1,32 @@
 module PolymorphicIntegerType
   module PolymorphicArrayValueExtension
-    def type_to_ids_mapping
-      super.tap do |result|
-        association = @associated_table.send(:association)
-        klass = association.active_record
-        name = association.name
 
-        if klass.respond_to?("#{name}_type_mapping")
-          result.transform_keys! do |key|
-            klass.send("#{name}_type_mapping").key(key)
-          end
+    # original method:
+    # def type_to_ids_mapping
+    #   default_hash = Hash.new { |hsh, key| hsh[key] = [] }
+    #   result = values.each_with_object(default_hash) do |value, hash|
+    #     hash[klass(value).polymorphic_name] << convert_to_id(value)
+    #   end
+    # end
+
+    def type_to_ids_mapping
+      association = @associated_table.send(:association)
+      name = association.name
+      default_hash = Hash.new { |hsh, key| hsh[key] = [] }
+
+      values.each_with_object(default_hash) do |value, hash|
+        if association.active_record.respond_to?("#{name}_type_mapping")
+          mapping = association.active_record.send("#{name}_type_mapping")
+          klass = klass(value)
+          key ||= mapping.key(klass.polymorphic_name)
+          key ||= mapping.key(klass.sti_name)
+          key ||= mapping.key(klass.base_class.to_s)
+          key ||= mapping.key(klass.base_class.sti_name)
+
+          hash[key] << convert_to_id(value)
+        else
+          hash[klass.polymorphic_name] << convert_to_id(value)
         end
-        result
       end
     end
   end

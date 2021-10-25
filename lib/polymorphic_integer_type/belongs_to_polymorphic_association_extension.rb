0d472694d0cefc76516e4e30aab3c21f5b167f19
Joseph Morain <joseph.morain@clio.com>
2021-10-25 13:46:34 -0400

    add force:false

--- lib/polymorphic_integer_type/belongs_to_polymorphic_association_extension.rb
+++ lib/polymorphic_integer_type/belongs_to_polymorphic_association_extension.rb
@@ -1,7 +1,7 @@
 module ActiveRecord
   module Associations
     class BelongsToPolymorphicAssociation < BelongsToAssociation
-      private def replace_keys(record)
+      private def replace_keys(record, force: false)
         super
         unless record.nil?
           owner[reflection.foreign_type] = record.class.base_class
